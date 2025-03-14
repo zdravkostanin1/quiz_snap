99f26470e8b407d047fc445fefda0aa03a26b629
zdravkostanin1 <zdravko.stanin@gmail.com>
2025-03-14 14:24:31 +0200

    Added the LucideIcons package for more nicer icons. Created the QuickActions widget.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lucide_icons/lucide_icons.dart';
 
 void main() {
   runApp(const App());
@@ -40,9 +41,32 @@ class QuizPage extends StatelessWidget {
               ),
               SizedBox(height: 15.0),
               QuizCard(hasBegunQuiz: true),
+              SizedBox(height: 15.0),
+              QuickActions(),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class QuickActions extends StatelessWidget {
+  const QuickActions({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(LucideIcons.x, color: Colors.red, size: 35.0),
+          SizedBox(width: 15.0),
+          Icon(LucideIcons.heart, color: Colors.black, size: 30.0),
+          SizedBox(width: 15.0),
+          Icon(LucideIcons.rotateCcw, color: Colors.green, size: 30.0),
+        ],
       ),
     );
   }
@@ -68,17 +92,14 @@ class QuizCard extends StatelessWidget {
                       color: Colors.white,
                       borderRadius: BorderRadius.circular(20),
                     ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Question 1 of 10",
-                          style: TextStyle(
-                            color: Colors.blueGrey,
-                            fontSize: 17.0,
-                          ),
+                    child: Center(
+                      child: Text(
+                        "Question 1 of 10",
+                        style: TextStyle(
+                          color: Colors.blueGrey,
+                          fontSize: 17.0,
                         ),
-                      ],
+                      ),
                     ),
                   ),
                   SizedBox(height: 10.0),
@@ -160,7 +181,9 @@ class QuizCard extends StatelessWidget {
                                 width: 130,
                                 height: 50,
                                 decoration: BoxDecoration(
-                                  color: Colors.blueGrey,
+                                  color: Color(
+                                    0xffA9B5DF,
+                                  ), // TODO: When an answer is selected, change color to a brighther blue
                                   borderRadius: BorderRadius.circular(10.0),
                                 ),
                                 child: Center(
