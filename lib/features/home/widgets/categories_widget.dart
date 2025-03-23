2466c5fb68a70a14585593816aa3f4752d56f2be
zdravkostanin1 <zdravko.stanin@gmail.com>
2025-03-24 00:23:48 +0200

    Removing a Row widget to fix overflow issues with the category.

--- lib/features/home/widgets/categories_widget.dart
+++ lib/features/home/widgets/categories_widget.dart
@@ -9,24 +9,18 @@ class CategoriesWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<HomeBloc, HomeState>(
       builder: (context, state) {
-        return Row(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                color: Color(0xff7D0A0A),
-                borderRadius: BorderRadius.circular(5.0),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(5.0),
-                child: Text(
-                  state.quizzes[state.currentQuestionIndex].category,
-                  style: TextStyle(color: Colors.white, fontSize: 15.5),
-                  maxLines: 2,
-                ),
-              ),
+        return Container(
+          decoration: BoxDecoration(
+            color: Color(0xff7D0A0A),
+            borderRadius: BorderRadius.circular(5.0),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Text(
+              state.quizzes[state.currentQuestionIndex].category,
+              style: TextStyle(color: Colors.white, fontSize: 15.5),
             ),
-            SizedBox(width: 10.0),
-          ],
+          ),
         );
       },
     );
